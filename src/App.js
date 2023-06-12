8946338cfae6d22e74b0afbe42c475273b2ff203
Alexandr1807 <alexandrcyganov18@gmail.com>
2023-06-12 19:25:43 +0500

    add Users page component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import UsersTable from './components/UsersTable/UsersTable';
 
 function App() {
+
   return (
-    <div className="App">
-      
+    <div className="container">
+      <UsersTable />
     </div>
   );
 }
---
+++ src/components/UsersTable/UsersTable.js
@@ -0,0 +1,86 @@
+import React, {useState, useEffect} from 'react'
+import {  Avatar, Button, List, Skeleton  } from 'antd'
+
+function UsersTable() {
+  const [initLoading, setInitLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [list, setList] = useState([]);
+  const count = 10;
+
+  let fakeDataUrl = `https://randomuser.me/api/?results=${count}`
+
+  useEffect(() => {
+    fetch(fakeDataUrl)
+      .then((res) => res.json())
+      .then((res) => {
+        setInitLoading(false);
+        setData(res.results);
+        setList(res.results);
+        console.log(res.results)
+      });
+  }, []);
+
+  const onLoadMore = () => {
+    setLoading(true);
+    setList(
+      data.concat(
+        [...new Array(count)].map(() => ({
+          loading: true,
+          name: {},
+          picture: {},
+        })),
+      ),
+    );
+
+    fetch(fakeDataUrl)
+      .then((res) => res.json())
+      .then((res) => {
+        const newData = data.concat(res.results);
+        setData(newData);
+        setList(newData);
+        setLoading(false);
+        window.dispatchEvent(new Event('resize'));
+      });
+  };
+
+  const loadMore =
+    !initLoading && !loading ? (
+      <div
+        style={{
+          textAlign: 'center',
+          marginTop: 12,
+          height: 32,
+          lineHeight: '32px',
+        }}
+      >
+        <Button onClick={onLoadMore}>More</Button>
+      </div>
+    ) : null;
+    
+  return (
+    <List
+      className="demo-loadmore-list"
+      loading={initLoading}
+      itemLayout="horizontal"
+      loadMore={loadMore}
+      dataSource={list}
+      renderItem={(item) => (
+        <List.Item
+          actions={[<a key="list-loadmore-more">More</a>]}
+        >
+          <Skeleton avatar title={false} loading={item.loading} active>
+            <List.Item.Meta
+              avatar={<Avatar src={item.picture.large} />}
+              title={<a href="https://ant.design">{item.name?.last}</a>}
+              description={item.location?.country}
+            />
+          </Skeleton>
+        </List.Item>
+      )}
+    />
+  );
+};
+
+
+export default UsersTable
