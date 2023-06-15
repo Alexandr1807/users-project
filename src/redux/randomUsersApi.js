c23add1f181bbf511be18a3c6f4654b06e3f609f
Alexandr1807 <alexandrcyganov18@gmail.com>
2023-06-15 11:16:05 +0500

    add file api with urls object for using in randomUsersApi js file

--- src/redux/randomUsersApi.js
+++ src/redux/randomUsersApi.js
@@ -1,8 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { api } from '../utils/api'; 
 
 export const randomUsersApi = createApi({
     reducerPath: 'randomUser',
-    baseQuery: fetchBaseQuery({baseUrl: 'https://randomuser.me/api/'}),
+    baseQuery: fetchBaseQuery({baseUrl: api.randomUsersApiUrl}),
     endpoints: (build) => ({
         getUsers: build.query({
             query: (result) => `?results=${result}`
